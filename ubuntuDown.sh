b711119c2084753310034355f67d1495879735a2
Cyprian <cyprian.bartosik@cirvianum.cat>
2025-12-17 22:37:14 +0100

    afegim el /vendor i el .env, perque si no el server no funca

--- ubuntuDown.sh
+++ ubuntuDown.sh
@@ -3,7 +3,6 @@
 #Access to server folder set up the server#
 echo === STOPPING SERVER ===
 cd ./server
-pkill -f vite || echo "No Vite process"
 ./vendor/bin/sail down
 
 #Access to client folder to set up the client view#
